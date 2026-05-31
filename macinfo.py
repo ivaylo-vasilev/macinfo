d691c8bf42498a427ae339712a4d3ed38fd0e708
Ivaylo Vasilev <ivaylovasilev81@gmail.com>
2026-06-01 00:57:43 +0300

    added info header; updated macdb.json

--- macinfo.py
+++ macinfo.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+##############################
+# macinfo #
+# ======= #
+# Get the vendor name by MAC address using a Python script and a database of known vendors
+# Copyright (c)2026 Ivaylo Vasilev. Released under the MIT License; see LICENSE for details.
+# Author: Ivaylo Vasilev
+##############################
+
 import argparse
 import re
 import json
